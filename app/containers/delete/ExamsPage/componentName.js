816264324be4f30baf8fb308b8f8d8fdb73e745a
Gurusharan Noola <gurusharannoola@Gurusharans-M1-MacBook-Pro.local>
2024-05-24 21:55:54 +0530

    Clean up

--- app/containers/delete/ExamsPage/componentName.js
+++
@@ -1,6 +0,0 @@
-import { capitalizeString } from "../../helpers/utils";
-
-export const componentNameSingular = 'exam' //change for new component in lowercase
-export const componentName = `${componentNameSingular}s` //change for new component plural
-export const componentNameCaps = componentName.toUpperCase();
-export const componentNameCapitalize= capitalizeString(componentName);
--- app/containers/delete/SubjectsPage/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    //compoent configs here
-}
